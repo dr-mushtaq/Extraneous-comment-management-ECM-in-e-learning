cc42752cff8d78408ce8fbd86867d7b46dbaa76f
Izhar Ul Haq <iizharulhaq9@gmail.com>
2022-12-13 23:10:56 +0500

    This is the first commit to see the if it is working

---
+++ chatBOT.py
@@ -0,0 +1,9 @@
+# Import Required Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+import plotly as pt
+#Created By Izhar Khan Khattak#
+st.header("Izhar Khan Khattak")
+st.write("Hello World!!!")
